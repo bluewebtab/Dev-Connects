42c23136a2512c80a131fad7a6144a652f398aa6
Ronny <ronny.icne@gmail.com>
2018-11-03 12:14:04 -0700

    Posts & Comments

--- client/src/App.js
+++ client/src/App.js
@@ -21,7 +21,11 @@ import AddExperience from './components/add-credentials/AddExperience';
 import AddEducation from './components/add-credentials/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import Posts from './components/posts/Posts';
+import Post from './components/post/Post';
+
 import NotFound from './components/not-found/NotFound';
+
 
 import './App.css';
 import { clearCurrentProfile } from './actions/profileActions';
@@ -94,6 +98,20 @@ class App extends Component {
          exact
          path = "/add-education"
          component = {AddEducation}
+          />
+        </Switch>
+        <Switch>
+        <PrivateRoute
+         exact
+         path = "/feed"
+         component = {Posts}
+          />
+        </Switch>
+        <Switch>
+        <PrivateRoute
+         exact
+         path = "/post/:id"
+         component = {Post}
           />
         </Switch>
         <Route exact path = "/not-found" component = {NotFound} />
--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb://ronny:ronny@ds121980.mlab.com:21980/devconnector",
-  secretOrKey: 'secret'
-};
+if(process.env.NODE_ENV === 'production'){
+  module.exports = require('./keys_prod');
+}else {
+  module.exports = require('./keys_dev')
+}
